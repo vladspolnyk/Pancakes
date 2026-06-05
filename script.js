517881c0149fb72f1db2de2162fbcc6283899617
vladspolnyk <vladspolnik7@gmail.com>
2026-06-05 20:37:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-}
-
-h1 {
-    text-align: center;
-}
-
-h2 {
-    text-align: center;
-}
-
-main {
-    margin: 0 auto;
-    background-color: white;
-    padding: 20px;
-    max-width: 500px;
-}
-
-.btn {
-    background-color: #ff6b6b;
-    border: none;
-    color: white;
-    padding: 10px 20px;
-    border-radius: 5px;
-    display: block;
-    margin: 30px auto 10px auto;
-    cursor: pointer;
-}
-
-img {
-    width: 100%;
-    border-radius: 8px;
-    margin-bottom: 25px;
-}
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector(".btn").onclick = () => {
+        if (document.body.style.backgroundColor === "lightblue") {
+            document.body.style.backgroundColor ="#f7f7f7";
+        }
+        else {
+            document.body.style.backgroundColor = "lightblue";
+        }
+    };
+});
