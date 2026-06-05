4e63e7b52903e13e6df75882035382b9cd28e286
vladspolnyk <vladspolnik7@gmail.com>
2026-06-05 20:36:47 +0200

    Update style.css

--- style.css
+++ style.css
@@ -1,1 +1,33 @@
+}
 
+h1 {
+    text-align: center;
+}
+
+h2 {
+    text-align: center;
+}
+
+main {
+    margin: 0 auto;
+    background-color: white;
+    padding: 20px;
+    max-width: 500px;
+}
+
+.btn {
+    background-color: #ff6b6b;
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 5px;
+    display: block;
+    margin: 30px auto 10px auto;
+    cursor: pointer;
+}
+
+img {
+    width: 100%;
+    border-radius: 8px;
+    margin-bottom: 25px;
+}
